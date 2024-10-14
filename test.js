cb63401643a6e6fa61ac262d2ce95eaf665d47c2
Vibhor Jaiswal <69683278+VibhorJaiswal@users.noreply.github.com>
2024-10-14 22:10:19 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+let let = 5;
+
+
+function test() {
+    return;
+}
+
+const element = <div>Hello World</div>; 
+
+// No inline configuration comment
+console.log("This will be ignored by ESLint"); 
